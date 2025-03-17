cc39a4a8c604c426f6c4f6820faac1a3ef8238aa
Kgermando <katakugermain@gmail.com>
2025-03-17 12:26:57 +0100

    refactor: clean up imports and improve HTML structure in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router'; 
+
 import { NavbarComponent } from '../../common/navbar/navbar.component';
 import { FooterComponent } from '../../common/footer/footer.component';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterModule,
+    RouterModule, 
     NavbarComponent,
     FooterComponent, 
   ],
