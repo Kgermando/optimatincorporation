f95320a80a9fb947ff4e93cc8fe98848d56c428f
Kgermando <katakugermain@gmail.com>
2025-03-18 19:53:30 +0100

    Add team

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../common/footer/footer.component';
 import { NavbarComponent } from '../../common/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { SMTPClient } from 'emailjs';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 
 @Component({
   selector: 'app-contact',
@@ -9,10 +12,38 @@ import { RouterModule } from '@angular/router';
     RouterModule,
     NavbarComponent,
     FooterComponent, 
+    FormsModule, // Add FormsModule to imports
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  emailForm = {
+    text: '',
+    from: '',  
+    subject: ''
+  };
 
+  async sendEmail() {
+    const { text, from, subject } = this.emailForm; // Use form data
+    const client = new SMTPClient({
+      user: 'contact@optimatincorporation.com',
+      password: 'optimat@2025',
+      host: 'mail.gandi.net',
+      ssl: true,
+    });
+
+    try {
+      const message = await client.sendAsync({
+        text: text,
+        from: from,
+        to: 'contact@optimatincorporation.com',
+        cc: 'contact@optimatincorporation.com',
+        subject: subject,
+      });
+      console.log(message);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
