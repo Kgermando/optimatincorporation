12c905fcf230e26b556402c49f257eb050b324a2
Germain kataku <katakugermain@gmail.com>
2025-07-19 07:42:48 +0100

    fix(environment): remove trailing slash from urlFile for consistency

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://website-api-production-b6bb.up.railway.app/api',
-    urlFile: 'https://website-api-production-b6bb.up.railway.app/'
+    urlFile: 'https://website-api-production-b6bb.up.railway.app'
 };
