57779512cd2ae30e8c292d72cd70a60b59f0a582
Germain Kataku <katakugermain@gmail.com>
2026-05-12 10:12:32 +0100

    Update Angular configuration and environment settings
    - Added allowedCommonJsDependencies for quill-delta in angular.json
    - Updated production and development environment configurations
    - Removed obsolete production environment file
    - Added new development environment file
    - Defined new server routes for blog and admin sections

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,34 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'realisations/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/portfolio/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/services/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/blog/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/team/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/users/:id/edit',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
